9d6562d4d0a8aeb204b06652a68e4751593daa1c
Sohel Rana <sohelrana130523@gmail.com>
2024-11-19 21:39:20 +0000

    Navbar design done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import { RiMenu2Fill } from "react-icons/ri";
 import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
-
+  const [toggle, setToggle] = useState(true);
   return (
     <>
       <nav className={`w-full flex justify-between items-center py-6`}>
@@ -14,10 +14,37 @@ const Navbar = () => {
           {navLinks.map((navlink) => (<li
             key={navlink.id}
             className='font-poppins cursor-pointer text-white'
-          >{navlink.title}</li>))}
+          >
+            <a href={`#${navlink.id}`}>
+              {navlink.title}
+            </a>
+          </li>))}
         </ul>
-        
-        
+        <button
+
+          className='sm:hidden block text-white'
+          onClick={() => setToggle((prev) => !prev)}
+        >
+          {
+            toggle ? <RiMenu2Fill /> : <RxCross1 />
+          }
+        </button>
+
+        <div className={`sm:hidden ${toggle ? 'hidden' : 'flex'} bg-black-gradient p-6 min-w-[140px] absolute top-20 right-0 mx-4 my-2 rounded-xl sidebar`}>
+          <div className="">
+            <ul className="list-none flex flex-col justify-start  flex-1 gap-4">
+              {navLinks.map((navlink) => (<li
+                key={navlink.id}
+                className='font-poppins cursor-pointer text-white'
+              >
+                <a href={`#${navlink.id}`}>
+                  {navlink.title}
+                </a>
+              </li>))}
+            </ul>
+          </div>
+        </div>
+
       </nav>
     </>
   );
